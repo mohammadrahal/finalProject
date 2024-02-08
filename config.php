25a5b4fa14da38ee2c099b7221f4aa3e241ed77b
Mohammad Rahal <rahalmohamad21@gmail.com>
2024-02-08 18:26:54 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$localhost ="localhost";
+$user = "root";
+$password = "";
+$dbName= "library";
+$conn = mysqli_connect($localhost, $user, $password, $dbName);
+
+if(!$conn){  
+        die('Could not connect: ' . mysqli_error());  
+      }  
+    //   echo 'WOWOWOWOW Connected successfully';  
+      mysqli_close($conn); 
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include "config.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Library</title>
+</head>
+<body>
+  <header>
+<h1>header</h1>
+  </header>
+
+  <section class="">
+    <h1>section 1</h1>
+</section>
+<section class="">
+<h1>section 2</h1>
+</section>
+<section class="">
+<h1>section 3</h1>
+</section>
+<section class="">
+<h1>section 4</h1>
+</section>
+
+<footer class="">
+<h1>footer</h1>
+</footer>
+<!-- js -->
+    <script src="js/app.js"></script>
+</body>
+</html>
